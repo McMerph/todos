d4d4dac78a7891c961f49de981b8273649b3b9c9
Aleksandr Sidorenko <mcmerph@gmail.com>
2018-04-05 13:10:26 +1000

    async/await init

--- backend/misc/init.js
+++ backend/misc/init.js
@@ -1,25 +1,24 @@
 require('isomorphic-fetch');
 
-const sendRequest = (method, entity) => {
-  return fetch('http://localhost:48702/todos-webapi', {
+const sendRequest = (method, entity) =>
+  fetch('http://localhost:48702/todos-webapi', {
     headers: {
       "content-type": "application/json"
     },
     method,
     body: JSON.stringify(entity)
-  })
-};
+  });
 
-const sendChainedCreateRequests = (quantity) => {
-  const next = (i = 0) =>
-    sendRequest("post", { text: `Todo ${i}`, completed: (i++) % 2 === 0 })
-      .then(() => (i < quantity) && next(i));
-
-  next();
-};
-
-sendRequest('delete')
-  .then(() => sendRequest('post', { text: 'Work', completed: true }))
-  .then(() => sendRequest('post', { text: 'Learn', completed: false }))
-  .then(() => sendRequest('post', { text: 'Drink beer', completed: false }))
-  .then(() => sendChainedCreateRequests(21));
+(async () => {
+  try {
+    await sendRequest('delete');
+    await sendRequest('post', { text: 'Work', completed: true });
+    await sendRequest('post', { text: 'Learn', completed: false });
+    await sendRequest('post', { text: 'Drink beer', completed: false });
+    for (let i = 0; i < 21; i++) {
+      await sendRequest("post", { text: `Todo ${i}`, completed: i % 2 === 0 });
+    }
+  } catch (error) {
+    console.error("There has been an error", error);
+  }
+})();
